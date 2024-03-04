f53c4f8cdc6aefc0efe3b1e59de28d2f99772f19
jmh <thinghoon@icloud.com>
2024-03-04 17:23:13 +0900

    first commit

---
+++ proto/src/test/java/team2/proto/ProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package team2.proto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
