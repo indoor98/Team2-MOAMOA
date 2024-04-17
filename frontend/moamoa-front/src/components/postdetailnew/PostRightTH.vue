c6f7c41c9ea75851ddc7ea66985265f314765860
jmh <thinghoon@icloud.com>
2024-04-17 19:27:28 +0900

    commentnew 꾸미기

---
+++ frontend/moamoa-front/src/components/postdetailnew/PostRightTH.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from "axios";
+
+const loadComments = async () => {
+  try {
+    // 게시글 정보 불러오기
+    const postId = window.location.pathname.split('/').pop();
+
+    // API 요청 보내기
+    const response = await axios.get()
+
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+</script>
+
+<template>
+<button>
+  드가자
+</button>
+</template>
+
+<style scoped>
+
+</style>
